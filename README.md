aadea257a946b5e975b9ce4091fb7cb9061fa290
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2019-11-26 14:07:56 +0000

    Adding more test cases and .eslintrc

--- README.md
+++ README.md
@@ -1,5 +1,34 @@
 # URI Swiss Army Knife Utilities
 
+## URL Validation
+
+A complete definition of what constitutes a valid URL can be found in [RFC 3986](http://tools.ietf.org/html/rfc3986) and [RFC 3987](http://tools.ietf.org/html/rfc3987). The short version is that a valid URL must, at minimum, consist of a scheme (`https://`, `http://ftp://`, `http://gopher://`) and a host name. If it does not, validation should fail, and the browser should throw an error.
+
+A URL string is a structured string containing multiple meaningful components. When parsed, a URL object is returned containing properties for each of these components.
+
+The Node.js `url` module provides two APIs for working with URLs: a legacy API that is Node.js specific, and a newer API that implements the same `WHATWG` URL [Standard](https://url.spec.whatwg.org/) used by web browsers.
+
+```
+┌────────────────────────────────────────────────────────────────────────────────────────────────┐
+│                                              href                                              │
+├──────────┬──┬─────────────────────┬────────────────────────┬───────────────────────────┬───────┤
+│ protocol │  │        auth         │          host          │           path            │ hash  │
+│          │  │                     ├─────────────────┬──────┼──────────┬────────────────┤       │
+│          │  │                     │    hostname     │ port │ pathname │     search     │       │
+│          │  │                     │                 │      │          ├─┬──────────────┤       │
+│          │  │                     │                 │      │          │ │    query     │       │
+"  https:   //    user   :   pass   @ sub.example.com : 8080   /p/a/t/h  ?  query=string   #hash "
+│          │  │          │          │    hostname     │ port │          │                │       │
+│          │  │          │          ├─────────────────┴──────┤          │                │       │
+│ protocol │  │ username │ password │          host          │          │                │       │
+├──────────┴──┼──────────┴──────────┼────────────────────────┤          │                │       │
+│   origin    │                     │         origin         │ pathname │     search     │ hash  │
+├─────────────┴─────────────────────┴────────────────────────┴──────────┴────────────────┴───────┤
+│                                              href                                              │
+└────────────────────────────────────────────────────────────────────────────────────────────────┘
+(all spaces in the "" line should be ignored — they are purely for formatting)
+```
+
 <dl>
 <dt><a href="helpers/README.md#isValidURI">isValidURI(url)</a> ⇒ <code>Boolean</code></dt>
 <dd><p>Validate if a passed string is a valud URI according to: <a href="https://gist.github.com/dperini/729294">https://gist.github.com/dperini/729294</a></p>
--- test/data/URIs.match.js
+++ test/data/URIs.match.js
@@ -1,194 +1,214 @@
 module.exports = [
-    {
-        url: "http://foo.com/blah_blah",
-        hostname: "foo.com",
-        domain: "foo.com",
-        domainName: "foo"
-    },
-    {
-        url: "http://foo.com/blah_blah/",
-        hostname: "foo.com",
-        domain: "foo.com",
-        domainName: "foo"
-    },
-    {
-        url: "http://foo.com/blah_blah_(wikipedia)",
-        hostname: "foo.com",
-        domain: "foo.com",
-        domainName: "foo"
-    },
-    {
-        url: "http://foo.com/blah_blah_(wikipedia)_(again)",
-        hostname: "foo.com",
-        domain: "foo.com",
-        domainName: "foo"
-    },
-    {
-        url: "http://www.example.com/wpstyle/?p=364",
-        hostname: "example.com",
-        domain: "example.com",
-        domainName: "example"
-    },
-    {
-        url: "https://www.example.com/foo/?bar=baz&inga=42&quux",
-        hostname: "example.com",
-        domain: "example.com",
-        domainName: "example"
-    },
-    {
-        url: "http://userid:password@example.com:8080",
-        hostname: "example.com",
-        domain: "example.com",
-        domainName: "example"
-    },
-    {
-        url: "http://userid:password@example.com:8080/",
-        hostname: "example.com",
-        domain: "example.com",
-        domainName: "example"
-    },
-    {
-        url: "http://userid@example.com",
-        hostname: "example.com",
-        domain: "example.com",
-        domainName: "example"
-    },
-    {
-        url: "http://userid@example.com/",
-        hostname: "example.com",
-        domain: "example.com",
-        domainName: "example"
-    },
-    {
-        url: "http://userid@example.com:8080",
-        hostname: "example.com",
-        domain: "example.com",
-        domainName: "example"
-    },
-    {
-        url: "http://userid@example.com:8080/",
-        hostname: "example.com",
-        domain: "example.com",
-        domainName: "example"
-    },
-    {
-        url: "http://userid:password@example.com",
-        hostname: "example.com",
-        domain: "example.com",
-        domainName: "example"
-    },
-    {
-        url: "http://userid:password@example.com/",
-        hostname: "example.com",
-        domain: "example.com",
-        domainName: "example"
-    },
-    {
-        url: "http://142.42.1.1/",
-        hostname: "142.42.1.1",
-        domain: "142.42.1.1",
-        domainName: "142.42.1.1"
-    },
-    {
-        url: "http://142.42.1.1:8080/",
-        hostname: "142.42.1.1",
-        domain: "142.42.1.1",
-        domainName: "142.42.1.1"
-    },
-    {
-        url: "http://foo.com/blah_(wikipedia)#cite-1",
-        hostname: "foo.com",
-        domain: "foo.com",
-        domainName: "foo"
-    },
-    {
-        url: "http://foo.com/blah_(wikipedia)_blah#cite-1",
-        hostname: "foo.com",
-        domain: "foo.com",
-        domainName: "foo"
-    },
-    {
-        url: "http://foo.com/unicode_(✪)_in_parens",
-        hostname: "foo.com",
-        domain: "foo.com",
-        domainName: "foo"
-    },
-    {
-        url: "http://foo.com/(something)?after=parens",
-        hostname: "foo.com",
-        domain: "foo.com",
-        domainName: "foo"
-    },
-    {
-        url: "http://code.google.com/events/#&product=browser",
-        hostname: "code.google.com",
-        domain: "google.com",
-        domainName: "google"
-    },
-    {
-        url: "http://j.mp",
-        hostname: "j.mp",
-        domain: "j.mp",
-        domainName: "j"
-    },
-    {
-        url: "ftp://foo.co.uk/baz",
-        hostname: "foo.co.uk",
-        domain: "foo.co.uk",
-        domainName: "foo"
-    },
-    {
-        url: "http://foo.co.uk/?q=Test%20URL-encoded%20stuff",
-        hostname: "foo.co.uk",
-        domain: "foo.co.uk",
-        domainName: "foo"
-    },
-    {
-        url: "http://-.~_!$&'()*+,;=:%40:80%2f::::::@example.com",
-        hostname: "example.com",
-        domain: "example.com",
-        domainName: "example"
-    },
-    {
-        url: "http://1337.net",
-        hostname: "1337.net",
-        domain: "1337.net",
-        domainName: "1337"
-    },
-    {
-        url: "http://foo.bar.ac.uk",
-        hostname: "foo.bar.ac.uk",
-        domain: "bar.ac.uk",
-        domainName: "bar"
-    },
-    {
-        url: "http://bar.geek.nz",
-        hostname: "bar.geek.nz",
-        domain: "bar.geek.nz",
-        domainName: "bar"
-    },
-    {
-        url: "http://a.b-c.de",
-        hostname: "a.b-c.de",
-        domain: "b-c.de",
-        domainName: "b-c"
-    },
-    {
-        url: "http://github.io/@ahmadassaf",
-        hostname: "github.io",
-        domain: "github.io",
-        domainName: "github"
-    },
-    {
-        url: "http://223.255.255.254",
-        hostname: "223.255.255.254",
-        domain: "223.255.255.254",
-        domainName: "223.255.255.254"
-    },
-    {
-        url: "http://2001:0db8:85a3:0000:0000:8a2e:0370:7334",
-        hostname: "2001:0db8:85a3:0000:0000:8a2e:0370:7334",
-        domain: "2001:0db8:85a3:0000:0000:8a2e:0370:7334",
-        domainName: "2001:0db8:85a3:0000:0000:8a2e:0370:7334"
-    }
-]
+  {
+    domain: "foo.com",
+    domainName: "foo",
+    hostname: "foo.com",
+    url: "http://foo.com/blah_blah"
+  },
+  {
+    domain: "foo.com",
+    domainName: "foo",
+    hostname: "foo.com",
+    url: "http://foo.com/blah_blah/"
+  },
+  {
+    domain: "foo.com",
+    domainName: "foo",
+    hostname: "foo.com",
+    url: "http://foo.com/blah_blah_(wikipedia)"
+  },
+  {
+    domain: "foo.com",
+    domainName: "foo",
+    hostname: "foo.com",
+    url: "http://foo.com/blah_blah_(wikipedia)_(again)"
+  },
+  {
+    domain: "example.com",
+    domainName: "example",
+    hostname: "example.com",
+    url: "http://www.example.com/wpstyle/?p=364"
+  },
+  {
+    domain: "example.com",
+    domainName: "example",
+    hostname: "example.com",
+    url: "https://www.example.com/foo/?bar=baz&inga=42&quux"
+  },
+  {
+    domain: "example.com",
+    domainName: "example",
+    hostname: "example.com",
+    url: "http://userid:password@example.com:8080"
+  },
+  {
+    domain: "example.com",
+    domainName: "example",
+    hostname: "example.com",
+    url: "http://userid:password@example.com:8080/"
+  },
+  {
+    domain: "example.com",
+    domainName: "example",
+    hostname: "example.com",
+    url: "http://userid@example.com"
+  },
+  {
+    domain: "example.com",
+    domainName: "example",
+    hostname: "example.com",
+    url: "http://userid@example.com/"
+  },
+  {
+    domain: "example.com",
+    domainName: "example",
+    hostname: "example.com",
+    url: "http://userid@example.com:8080"
+  },
+  {
+    domain: "example.com",
+    domainName: "example",
+    hostname: "example.com",
+    url: "http://userid@example.com:8080/"
+  },
+  {
+    domain: "example.com",
+    domainName: "example",
+    hostname: "example.com",
+    url: "http://userid:password@example.com"
+  },
+  {
+    domain: "example.com",
+    domainName: "example",
+    hostname: "example.com",
+    url: "http://userid:password@example.com/"
+  },
+  {
+    domain: "142.42.1.1",
+    domainName: "142.42.1.1",
+    hostname: "142.42.1.1",
+    url: "http://142.42.1.1/"
+  },
+  {
+    domain: "142.42.1.1",
+    domainName: "142.42.1.1",
+    hostname: "142.42.1.1",
+    url: "http://142.42.1.1:8080/"
+  },
+  {
+    domain: "foo.com",
+    domainName: "foo",
+    hostname: "foo.com",
+    url: "http://foo.com/blah_(wikipedia)#cite-1"
+  },
+  {
+    domain: "foo.com",
+    domainName: "foo",
+    hostname: "foo.com",
+    url: "http://foo.com/blah_(wikipedia)_blah#cite-1"
+  },
+  {
+    domain: "foo.com",
+    domainName: "foo",
+    hostname: "foo.com",
+    url: "http://foo.com/unicode_(✪)_in_parens"
+  },
+  {
+    domain: "foo.com",
+    domainName: "foo",
+    hostname: "foo.com",
+    url: "http://foo.com/(something)?after=parens"
+  },
+  {
+    domain: "google.com",
+    domainName: "google",
+    hostname: "code.google.com",
+    url: "http://code.google.com/events/#&product=browser"
+  },
+  {
+    domain: "j.mp",
+    domainName: "j",
+    hostname: "j.mp",
+    url: "http://j.mp"
+  },
+  {
+    domain: "foo.co.uk",
+    domainName: "foo",
+    hostname: "foo.co.uk",
+    url: "ftp://foo.co.uk/baz"
+  },
+  {
+    domain: "foo.co.uk",
+    domainName: "foo",
+    hostname: "foo.co.uk",
+    url: "http://foo.co.uk/?q=Test%20URL-encoded%20stuff"
+  },
+  {
+    domain: "example.com",
+    domainName: "example",
+    hostname: "example.com",
+    url: "http://-.~_!$&'()*+,;=:%40:80%2f::::::@example.com"
+  },
+  {
+    domain: "1337.net",
+    domainName: "1337",
+    hostname: "1337.net",
+    url: "http://1337.net"
+  },
+  {
+    domain: "bar.ac.uk",
+    domainName: "bar",
+    hostname: "foo.bar.ac.uk",
+    url: "http://foo.bar.ac.uk"
+  },
+  {
+    domain: "bar.geek.nz",
+    domainName: "bar",
+    hostname: "bar.geek.nz",
+    url: "http://bar.geek.nz"
+  },
+  {
+    domain: "b-c.de",
+    domainName: "b-c",
+    hostname: "a.b-c.de",
+    url: "http://a.b-c.de"
+  },
+  {
+    domain: "github.io",
+    domainName: "github",
+    hostname: "github.io",
+    url: "http://github.io/@ahmadassaf"
+  },
+  {
+    domain: "223.255.255.254",
+    domainName: "223.255.255.254",
+    hostname: "223.255.255.254",
+    url: "http://223.255.255.254"
+  },
+  {
+    domain: "2001:0db8:85a3:0000:0000:8a2e:0370:7334",
+    domainName: "2001:0db8:85a3:0000:0000:8a2e:0370:7334",
+    hostname: "2001:0db8:85a3:0000:0000:8a2e:0370:7334",
+    url: "http://2001:0db8:85a3:0000:0000:8a2e:0370:7334"
+  },
+  {
+    domain: "npmjs.com",
+    domainName: "npmjs",
+    hostname: "npmjs.com",
+    url: "https://www.npmjs.com/package/extract-domain"
+  },
+  {
+    domain: "example.com",
+    domainName: "example",
+    hostname: "example.com",
+    url:
+      "http://www.example.com:80/path/to/myfile.html?key1=value1&key2=value2#SomewhereInTheDocument"
+  },
+  {
+    domain: "example.com",
+    domainName: "example",
+    hostname: "example.com",
+    url:
+      "http://user:password@example.com:80/path/to/myfile.html?key1=value1&key2=value2#SomewhereInTheDocument"
+  }
+];
