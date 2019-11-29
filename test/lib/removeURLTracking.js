c191ff490e738566d0b34aa9e9c0f3bf563e4720
ghnr <45798027+ghnr@users.noreply.github.com>
2019-11-29 18:42:34 +0000

    Add removeURLTracking tests

---
+++ test/lib/removeURLTracking.js
@@ -0,0 +1,105 @@
+const assert = require('assert');
+
+const urlUtils = require('../../index');
+
+describe('removeURLTracking function', () => {
+  
+  it('should return false for an invalid URI', () => {
+    assert.equal(
+      urlUtils.removeURLTracking(
+        'https://'
+      ), false
+    );
+  });
+
+  it('should return original URI if there are no tracking parameters', () => {
+    assert.equal(
+      urlUtils.removeURLTracking(
+        'https://foo.com/article/page_title'
+      ), 'https://foo.com/article/page_title'
+    );
+  });
+
+  it('should be able to remove a single tracking component of a given URI', () => {
+    assert.equal(
+      urlUtils.removeURLTracking(
+        'https://foo.com/page_title?utm_campaign=f4c93d76a953'
+      ), 'https://foo.com/page_title'
+    );
+  });
+
+  it('should remove multiple different tracking components of a given URI', () => {
+    assert.equal(
+      urlUtils.removeURLTracking(
+        'https://foo.com/page_title?utm_campaign=f4c93d76a953?fbclid=092c4759?ga_campaign=f4c93d76a953'
+      ), 'https://foo.com/page_title'
+    );
+  });
+
+  it('should remove multiple different tracking components of a given URI, which are joined using either "?" or "&"', () => {
+    assert.equal(
+      urlUtils.removeURLTracking(
+        'https://foo.com/page_title?utm_campaign=f4c93d76a953&fbclid=092c4759?ga_campaign=f4c93d76a953'
+      ), 'https://foo.com/page_title'
+    );
+  });
+
+  it('should remove only tracking parameters and leave valid parameters untouched', () => {
+    assert.equal(
+      urlUtils.removeURLTracking(
+        'https://foo.com/page_title?utm_campaign=f4c93d76a953?user_id=foo'
+      ), 'https://foo.com/page_title?user_id=foo'
+    );
+  });
+
+  it('should not remove unsafe tracking parameters if safe=true (default)', () => {
+    assert.equal(
+      urlUtils.removeURLTracking(
+        'https://foo.com/page_title?assetId=f4c93d76a953'
+      ), 'https://foo.com/page_title?assetId=f4c93d76a953'
+    );
+  });
+
+  it('should remove unsafe tracking parameters if safe=false', () => {
+    assert.equal(
+      urlUtils.removeURLTracking(
+        'https://foo.com/page_title?assetId=f4c93d76a953',
+        false
+      ), 'https://foo.com/page_title'
+    );
+  });
+
+  it('should remove unsafe tracking parameters if safe=false', () => {
+    assert.equal(
+      urlUtils.removeURLTracking(
+        'https://foo.com/page_title?assetId=f4c93d76a953',
+        false
+      ), 'https://foo.com/page_title'
+    );
+  });
+
+  it('should remove domain-specific tracking parameters', () => {
+    assert.equal(
+      urlUtils.removeURLTracking(
+        'https://facebook.com/page_title?comment_tracking=f4c93d76a953'
+      ), 'https://facebook.com/page_title'
+    );
+  });
+
+  it('should not remove domain-specific tracking parameters if domain file does not exist', () => {
+    assert.equal(
+      urlUtils.removeURLTracking(
+        'https://foo.com/page_title?comment_tracking=failing'
+      ), 'https://foo.com/page_title?comment_tracking=failing'
+    );
+  });
+
+  it('should remove both global and domain-specific parameters', () => {
+    assert.equal(
+      urlUtils.removeURLTracking(
+        'https://facebook.com/page_title?comment_tracking=f4c93d76a953?utm_campaign=092c4759'
+      ), 'https://facebook.com/page_title'
+    );
+  });
+
+});
